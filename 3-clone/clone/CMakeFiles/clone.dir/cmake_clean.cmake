053f2b228e8de225740c83a5ae58281225ce6d61
leomols97 <leomols@hotmail.com>
2022-03-15 16:11:00 +0100

    clone

---
+++ 3-clone/clone/CMakeFiles/clone.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clone.dir/main.c.o"
+  "clone"
+  "clone.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/clone.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
