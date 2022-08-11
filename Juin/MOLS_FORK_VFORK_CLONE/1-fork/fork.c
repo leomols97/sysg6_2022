ab5bb0e16af869fe0dfbb396997854254a13ed2c
mathieu <mathieu@pop-os.localdomain>
2022-08-11 21:26:29 +0200

    fork done

--- Juin/MOLS_FORK_VFORK_CLONE/1-fork/fork.c
+++ Juin/MOLS_FORK_VFORK_CLONE/1-fork/fork.c
@@ -8,6 +8,7 @@
 #include <sys/mman.h>
 #include <signal.h>
 #include <ctype.h>
+#include <string.h>
 
 
 
@@ -18,20 +19,13 @@
 
 void continueProgram()
 {
-	printf("\nPour continuer le programme, entrez 'c'\n");
+	printf("Pour continuer le programme, entrez 'continue' ou 'c' : ");
 	int c, numberCounter = 0, letterCounter = 0;
 while ((c = getchar()) != 'c')
-{
     if (isalpha(c))
-    {
         letterCounter++;
-    }
     else if (isdigit(c))
-    {
         numberCounter++;
-    }
-}
-
 }
 
 int lock_memory(char * address, size_t size)
@@ -64,10 +58,10 @@ void signal_handler(int signal_nb)
 
 // A normal C function that is executed as a thread
 // when its name is specified in pthread_create()
-void *threadCreation(void *vargp)
+void *threadCreation(void *arg)
 {
-    sleep(30);
-    printf("Printing GeeksQuiz from Thread \n");
+    printf("Fonction liée à la création de thread appelée \n");
+    sleep(50); // Ceci pour permettre d'avoir le temps de prouver que le fils n'hérite pas des threads du père ni en crée de nouveaux
     return NULL;
 }
 
@@ -97,7 +91,7 @@ int main(int argc, char **argv) {
 
                "<    haute priorité (non poli pour les autres utilisateurs) ;\n"
                "N    basse priorité (poli pour les autres utilisateurs) ;\n"
-               "L    avec ses pages verrouillées en mémoire (pour temps réel et entrées et sorties personnalisées) ;\n"
+               "L    les pages du processus sont verrouillées en mémoire;\n"
                "s    meneur de session ;\n"
                "l    possède plusieurs processus légers (« multi-thread », utilisant CLONE_THREAD comme NPTL pthreads le fait) ;\n"
                "+    dans le groupe de processus au premier plan.\n\n\n\n\n\n\n");
@@ -109,6 +103,8 @@ int main(int argc, char **argv) {
     // Récupérer la valeur de retour de la fonction créant le proocess fils
     int forkRetNum;
     
+    printf("PID du père = %d\n", getpid());
+    printf ("Ces 2 variables sont créées par le père :\n");
     printf("a = %d\n", a);
     printf("b = %d\n", b);
     
@@ -118,7 +114,7 @@ int main(int argc, char **argv) {
     if (lock_memory(dataLock, dataSize) == -1)
     	perror("Error with locking memory\n");
     else
-    	printf ("Memory locked in RAM\n");
+    	printf ("\nDe la mémoire a été réservée en RAM par le père\n");
 
     	
     /*signal(SIGINT, signal_handler);
@@ -131,37 +127,45 @@ int main(int argc, char **argv) {
     
     continueProgram();
     
-    pthread_t thread_id;
-    printf("Before creating a thread with the parent\n");
-    pthread_create(&thread_id, NULL, threadCreation, NULL);
-    printf("Thread created with the parent\n");
+    printf("\nCeci est avant que le père ne crée un thread\n\n");
+    continueProgram();
     
+    int i;
+    pthread_t tid;
+    // Let us create three threads
+    for (i = 0; i < 3; i++)
+        pthread_create(&tid, NULL, threadCreation, (void *)&tid);
+ 
+    printf("");
+    //printf("Le thread a été créé par le père\n");
+    
+    //continueProgram();
     forkRetNum = fork();
         
     if(forkRetNum == 0) { // La création du fils s'est-elle correctement produite ?
-        printf("I'm the child !\n");
+        printf("Le processus fils vient d'être créé. La suite est affichée par le fils.\n");
         // a = 10 but only the one of the chile. not the one of the parent
         a = a + 5;
-        printf("Now, a = %d only in the child\n", a);
+        printf("Maintenant, a = %d et ce, uniquement dans l'espace d'adressage du fils\n", a);
 
         // b = 10 but only the one of the chile. not the one of the parent
         b = b + 2;
-        printf("Now, b = %d only in the child\n", b);
+        printf("Maintenant, b = %d et ce, uniquement dans l'espace d'adressage du fils\n", b);
 
 	//lock_memory();
 
-        printf("PID = %d\n", getpid());
-        printf("PPID = %d\n", getppid());
-        printf("Sum a + b is %d.\n", a + b); // line b
-        printf("Dans une autre fenêtre de terminal, entrez la commande 'ps' pour voir quel process est en cours et plus d'informations à leurs propos !\n\n");
-        printf("Dans une autre fenêtre de terminal, entrez la commande 'cat /proc/$PID/status | grep VmLck' pour voir quel memory lock est alloué au process !\n\n");
-        printf("Dans une autre fenêtre de terminal, entrez la commande 'cat /proc/$PID/status' pour voir les informations du process !\n");
-        printf("VSZ est l'abréviation de Virtual Memory Size. C'est la quantité totale de mémoire à laquelle un processus peut hypothétiquement accéder."
-               + "Il tient compte de la taille du binaire lui-même, de toutes les bibliothèques liées et de toutes les allocations de pile ou de tas.\n");
-        printf("RSS signifie Resident Set Size et montre la quantité de RAM utilisée au moment de la sortie de la commande.
-               + "Il convient également de noter qu'il affiche toute la pile de mémoire physiquement allouée.\n\n");
+        printf("PID (du fils, donc) = %d\n", getpid());
+        printf("PID du père = %d\n", getppid());
+        printf("a + b = %d.\n", a + b);
+        printf("\nDans une autre fenêtre de terminal, entrez la commande 'ps' pour voir quel process est en cours et plus d'informations à leurs propos !\n\n");
+        printf("Sous la section 'Status', vous pouvez voir que le statut du père est 'SLl+'. Le 'L' signifie que de la mémoire est verrouillée en RAM par le process !\n\n");
+        printf("RSS signifie Resident Set Size et montre la quantité de RAM utilisée au moment de la sortie de la commande. "
+        	"Il convient également de noter qu'il affiche toute la pile de mémoire physiquement allouée.\n\n");
+        printf("VSZ est l'abréviation de Virtual Memory Size. C'est la quantité totale de mémoire à laquelle un processus peut hypothétiquement accéder. "
+                "Il tient compte de la taille du binaire lui-même, de toutes les bibliothèques liées et de toutes les allocations de pile ou de tas.\n");
+        printf("\n\nDans une autre fenêtre de terminal, entrez la commande 'cat /proc/$PID/status' pour voir les informations du process !\n");
         
-        sleep(30);
+        continueProgram();
         //while(1){} // Faire en sorte que le fils attende, mais en étant en état d'exécution. Un simple ps le montrera
         exit(0);
     }
@@ -176,11 +180,12 @@ int main(int argc, char **argv) {
     }
     // Parent code
     wait(0); // Pour éviter de faire du fils un zombie
+    printf ("Le fils est terminé\n");
     printf("PID = %d\n", getpid());
     printf("PPID = %d\n", getppid());
-    printf("Value of f is %d.\n", forkRetNum); // Indiquer la valeur de retour de la fonciton créant le process
     // La somme est bien de 13 et non plus ni moins puisque la somme fut faite par le fils, mais uniquement avec ses propres variables et non celles du père
-    printf("Sum is %d.\n", a + b); // line b
+    printf("a + b = %d.\n", a + b);
+    printf("Vu que a + b = 20 dans le fils et que a + b = 13 dans le père, cela prouve que l'espace d'adressage d'un process créé au moyen de fork n'est pas celui du père car il a été dupliqué par rapport à celui du père. Chaque process a donc ses propres variables,...\n");
     printf("Let's do a ps to see which process is currenlty running !");
     
     if (unlock_memory(dataLock, dataSize) == -1)
@@ -190,7 +195,7 @@ int main(int argc, char **argv) {
     	
     printf ("\n\nLe programme ne se termine pas pour laisser le temps de faire un ps et voir quels process sont en cours d'exécution. Pour le terminer, faites un 'kill $PID' dans une autre fenêtre de terminal ou faites un CTRL + C\n");
         
-    pthread_join(thread_id, NULL);
+    pthread_join(tid, NULL);
     printf("After Thread\n");
   
     while(1){} // Simplement pour faire attendre le père que l'on fasse un ps pour pouvoir voir son état
