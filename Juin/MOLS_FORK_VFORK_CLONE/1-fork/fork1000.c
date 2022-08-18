2a14797b7d4683f43e53dacdb1cd3f6a066f3edf
53212 <53212@etu.he2b.be>
2022-08-19 01:31:29 +0200

    The command to execute fork1000.c is correct

--- Juin/MOLS_FORK_VFORK_CLONE/1-fork/fork1000.c
+++ Juin/MOLS_FORK_VFORK_CLONE/1-fork/fork1000.c
@@ -141,7 +141,7 @@ int main(int argc, char **argv) {
     for (unsigned int i = 0; i < nbForks; i++)
     {
         write_into_file(fileName, fileNameSize, " Parent ");
-        forkRetNums[i] = vfork();
+        forkRetNums[i] = fork();
         
         write_into_file(fileName, fileNameSize, " Fils\n");
         if(forkRetNums[i] == 0)
